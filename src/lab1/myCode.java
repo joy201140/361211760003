2a45d4ceb9f422c55b59b8929656701615ca9dc4
kanyanatjoy <kanyanat.thaioon@gmail.com>
2018-06-26 12:09:09 +0700

    Hello

---
+++ src/lab1/myCode.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("kanyanat thaioon");
+        System.out.println("361211760003");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+    }//main
+
+
+
+}
+
